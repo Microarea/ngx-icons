7e4a3d79a7203047ed3003c9a6516b884fef0078
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-05-29 11:45:57 +0200

    v1.0.0

---
+++ esm5/lib/icons.module.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+import { NgModule } from '@angular/core';
+import { IconsComponent } from './icons.component';
+var TbIconsModule = /** @class */ (function () {
+    function TbIconsModule() {
+    }
+    TbIconsModule.decorators = [
+        { type: NgModule, args: [{
+                    imports: [],
+                    declarations: [IconsComponent],
+                    exports: [IconsComponent]
+                },] },
+    ];
+    return TbIconsModule;
+}());
+export { TbIconsModule };
+function TbIconsModule_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    TbIconsModule.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    TbIconsModule.ctorParameters;
+}
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaWNvbnMubW9kdWxlLmpzIiwic291cmNlUm9vdCI6Im5nOi8vQHRiL2ljb25zLyIsInNvdXJjZXMiOlsibGliL2ljb25zLm1vZHVsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBQUEsT0FBTyxFQUFFLFFBQVEsRUFBRSxNQUFNLGVBQWUsQ0FBQztBQUN6QyxPQUFPLEVBQUUsY0FBYyxFQUFFLE1BQU0sbUJBQW1CLENBQUM7Ozs7O2dCQUVsRCxRQUFRLFNBQUM7b0JBQ1IsT0FBTyxFQUFFLEVBQ1I7b0JBQ0QsWUFBWSxFQUFFLENBQUMsY0FBYyxDQUFDO29CQUM5QixPQUFPLEVBQUUsQ0FBQyxjQUFjLENBQUM7aUJBQzFCOzt3QkFSRDs7U0FTYSxhQUFhIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgTmdNb2R1bGUgfSBmcm9tICdAYW5ndWxhci9jb3JlJztcclxuaW1wb3J0IHsgSWNvbnNDb21wb25lbnQgfSBmcm9tICcuL2ljb25zLmNvbXBvbmVudCc7XHJcblxyXG5ATmdNb2R1bGUoe1xyXG4gIGltcG9ydHM6IFtcclxuICBdLFxyXG4gIGRlY2xhcmF0aW9uczogW0ljb25zQ29tcG9uZW50XSxcclxuICBleHBvcnRzOiBbSWNvbnNDb21wb25lbnRdXHJcbn0pXHJcbmV4cG9ydCBjbGFzcyBUYkljb25zTW9kdWxlIHsgfVxyXG4iXX0=
---
+++ esm5/public_api.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/*
+ * Public API Surface of icons
+ */
+export { IconsComponent } from './lib/icons.component';
+export { TbIconsModule } from './lib/icons.module';
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljX2FwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0B0Yi9pY29ucy8iLCJzb3VyY2VzIjpbInB1YmxpY19hcGkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUlBLCtCQUFjLHVCQUF1QixDQUFDO0FBQ3RDLDhCQUFjLG9CQUFvQixDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLypcclxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIGljb25zXHJcbiAqL1xyXG5cclxuZXhwb3J0ICogZnJvbSAnLi9saWIvaWNvbnMuY29tcG9uZW50JztcclxuZXhwb3J0ICogZnJvbSAnLi9saWIvaWNvbnMubW9kdWxlJztcclxuIl19
