de56c80c49c7476b87e67bbfb69af9e89cdf9b20
Luca <luca.bruni@microarea.it>
2022-02-21 14:51:37 +0100

    2.3.1, angular13

--- lib/icons.component.d.ts
+++ lib/icons.component.d.ts
@@ -1,4 +1,8 @@
+import * as i0 from "@angular/core";
 export declare class IconsComponent {
     _icon: string;
-    icon: any;
+    set icon(icon: any);
+    get icon(): any;
+    static ɵfac: i0.ɵɵFactoryDeclaration<IconsComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<IconsComponent, "m4-icon", never, { "icon": "icon"; }, {}, never, never>;
 }
--- lib/icons.module.d.ts
+++ lib/icons.module.d.ts
@@ -1,2 +1,7 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./icons.component";
 export declare class TbIconsModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<TbIconsModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<TbIconsModule, [typeof i1.IconsComponent], never, [typeof i1.IconsComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<TbIconsModule>;
 }
