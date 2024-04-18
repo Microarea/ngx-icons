d2fb22dbe6acad6b77c8e5f495f6c726e5cae2a2
Andrea-Mussi <andrea.mussi@zucchetti.com>
2024-04-18 12:46:22 +0200

    v2.4.14

--- lib/icons.module.d.ts
+++ lib/icons.module.d.ts
@@ -1,8 +1,6 @@
-import { ModuleWithProviders } from '@angular/core';
 import * as i0 from "@angular/core";
 import * as i1 from "./icons.component";
 export declare class TbIconsModule {
-    static forRoot(): ModuleWithProviders<TbIconsModule>;
     static ɵfac: i0.ɵɵFactoryDeclaration<TbIconsModule, never>;
     static ɵmod: i0.ɵɵNgModuleDeclaration<TbIconsModule, [typeof i1.IconsComponent], never, [typeof i1.IconsComponent]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<TbIconsModule>;
