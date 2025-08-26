01ae81b130313c191a99eb5bdd98bcd6ba833f30
Luca Bruni <luca.bruni@zucchetti.com>
2025-08-26 10:39:22 +0200

    v5.3.0+1 (angular 20)

--- lib/icons.component.d.ts
+++
@@ -1,8 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class IconsComponent {
-    _icon: string;
-    set icon(icon: any);
-    get icon(): any;
-    static ɵfac: i0.ɵɵFactoryDeclaration<IconsComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<IconsComponent, "m4-icon", never, { "icon": { "alias": "icon"; "required": false; }; }, {}, never, never, false, never>;
-}
