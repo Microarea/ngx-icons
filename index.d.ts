06f8ff80c3aa3ca58f2210caf8f25bbd71577cdb
Luca Bruni <luca.bruni@zucchetti.com>
2025-12-19 09:44:44 +0100

    v6.0.0+1

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,19 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@tb/icons" />
-export * from './public-api';
+import * as i0 from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
+
+declare class IconsComponent {
+    _icon: string;
+    set icon(icon: any);
+    get icon(): any;
+    static ɵfac: i0.ɵɵFactoryDeclaration<IconsComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<IconsComponent, "m4-icon", never, { "icon": { "alias": "icon"; "required": false; }; }, {}, never, never, false, never>;
+}
+
+declare class TbIconsModule {
+    static forRoot(): ModuleWithProviders<TbIconsModule>;
+    static ɵfac: i0.ɵɵFactoryDeclaration<TbIconsModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<TbIconsModule, [typeof IconsComponent], never, [typeof IconsComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<TbIconsModule>;
+}
+
+export { IconsComponent, TbIconsModule };
--- public-api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/icons.component';
-export * from './lib/icons.module';
